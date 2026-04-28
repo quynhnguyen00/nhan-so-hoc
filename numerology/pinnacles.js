5d0a26bb4e8d07b3121ac1773b8468f6a0534ca4
quynhnguyen00 <ngquynh889@gmail.com>
2026-04-28 22:37:31 +0700

    Tính 4 Đỉnh Cao đời người

--- numerology/pinnacles.js
+++ numerology/pinnacles.js
@@ -8,17 +8,21 @@ function calculatePinnacles(dob) {
     let m = parseInt(parts[1]);
     let y = parseInt(parts[0]);
 
-    let p1 = reduce(d + m);
-    let p2 = reduce(d + y);
-    let p3 = reduce(p1 + p2);
-    let p4 = reduce(m + y);
+    let p1 = reduceNumber(d + m);
+    let p2 = reduceNumber(d + y);
+    let p3 = reduceNumber(p1 + p2);
+    let p4 = reduceNumber(m + y);
 
-    return [p1, p2, p3, p4];
+    return {
+        p1, p2, p3, p4
+    };
 }
 
-function reduce(num) {
-    while(num > 9 && num !== 11 && num !== 22) {
-        num = num.toString().split("").reduce((a,b)=>a+parseInt(b),0);
+// chuẩn hoá số
+function reduceNumber(num) {
+    while (num > 9 && num !== 11 && num !== 22) {
+        num = num.toString().split("")
+            .reduce((a, b) => a + parseInt(b), 0);
     }
     return num;
 }
