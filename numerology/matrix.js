74a4b45aaf9010244f09f15bdf1b8c2d7b2a7cc0
quynhnguyen00 <ngquynh889@gmail.com>
2026-04-28 22:25:35 +0700

    Tạo ma trận Pythagoras, lấy mũi tên có và mũi tên trống

--- numerology/matrix.js
+++ numerology/matrix.js
@@ -16,3 +16,35 @@ function buildMatrix(digits) {
 
     return matrix;
 }
+
+function getArrows(matrix) {
+
+    const arrows = {
+        "1-2-3": "Trí tuệ",
+        "4-5-6": "Thực tế",
+        "7-8-9": "Tâm linh",
+        "1-4-7": "Kỷ luật",
+        "2-5-8": "Cảm xúc",
+        "3-6-9": "Sáng tạo",
+        "1-5-9": "Quyết tâm",
+        "3-5-7": "Nhạy bén"
+    };
+
+    let result = {
+        strong: [],
+        missing: []
+    };
+
+    for(let key in arrows) {
+        let nums = key.split("-");
+        let hasAll = nums.every(n => matrix[n] > 0);
+
+        if(hasAll) {
+            result.strong.push(arrows[key]);
+        } else {
+            result.missing.push(arrows[key]);
+        }
+    }
+
+    return result;
+}
